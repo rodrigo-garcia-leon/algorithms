0d4fa2953b650d468878cf3891bf1c8e292f8b80
Rodrigo García <yo@rodrigogarcia.me>
2022-04-12 22:37:50 +0200

    word transformation

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ source .venv/bin/activate
 ## lowest common ancestor
 
 <img src="https://s3.amazonaws.com/hr-assets/0/1527870675-1cfffe0a8a-LCASample.png">
+
+## word transformation
+
+https://onlinejudge.org/index.php?option=onlinejudge&Itemid=8&page=show_problem&problem=370
---
+++ src/word-transformation/word-transformation.py
@@ -0,0 +1,46 @@
+from collections import deque
+
+
+def wordTransformation(dictionary, words):
+    filteredDictionary = [x for x in dictionary if len(x) == len(words[0])]
+
+    def isNeighbour(first, second):
+        return len(set(first) - set(second)) == 1
+
+    def getNeighbours(word):
+        return [x for x in filteredDictionary if isNeighbour(word, x)]
+
+    visited = []
+    toVisit = deque([[words[0]]])
+    transformations = 0
+    while len(toVisit) > 0:
+        wordList = toVisit.popleft()
+        transformations = transformations + 1
+
+        nextWordList = []
+        for word in wordList:
+            visited.append(word)
+
+            neighbours = [x for x in getNeighbours(word) if x not in visited]
+            for neighbour in neighbours:
+                if neighbour == words[1]:
+                    return transformations
+                nextWordList.append(neighbour)
+
+        if len(nextWordList) != 0:
+            toVisit.append(nextWordList.copy())
+
+    return -1
+
+
+with open('input.txt', 'r') as f:
+    n = int(f.readline())
+    f.readline()
+
+    dictionary = []
+    while ((word := f.readline().strip()) != '*'):
+        dictionary.append(word)
+
+    while ((line := f.readline().strip()) != ''):
+        t = wordTransformation(dictionary, line.split(' '))
+        print(t)
